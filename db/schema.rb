ada19d5a9f7225c33ab55d34c872c8da2223fd2f
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-04-29 15:47:39 +0900

    deviseのusersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_04_28_063427) do
+ActiveRecord::Schema.define(version: 2021_04_29_064128) do
 
   create_table "tweets", charset: "utf8", force: :cascade do |t|
     t.string "name"
@@ -19,6 +18,18 @@ ActiveRecord::Schema.define(version: 2021_04_28_063427) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
