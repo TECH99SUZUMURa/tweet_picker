d1e371d2a488868ba2ef0b3fa3e379f3ef6c2695
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-04-29 18:26:15 +0900

    tweetsテーブルからnameカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_081351) do
+ActiveRecord::Schema.define(version: 2021_04_29_092511) do
 
   create_table "tweets", charset: "utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
